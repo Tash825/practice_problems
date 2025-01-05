3db27f70c5882bb915e6b9a7adf6e72bf372fbf8
Tash825 <78715311+Tash825@users.noreply.github.com>
2025-01-05 23:37:58 +0530

    Update and rename zombie_game to zombie_game.java

--- zombie_game.java
+++ zombie_game.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class HelloWorld {
+class Zombie_game {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int b = sc.nextInt();
