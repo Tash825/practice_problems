89683e552a63ab980605343c06297f8603f57651
Tash825 <78715311+Tash825@users.noreply.github.com>
2025-01-06 20:46:06 +0530

    Update remaining_time.java

--- remaining_time.java
+++ remaining_time.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
-        int m = sc.nextInt();
+        Scanner s = new Scanner(System.in);
+        int h = s.nextInt();
+        int m = s.nextInt();
         int mins = (h*60) + m;
         int remMin = (24*60) - mins;
         int remH = remMin/60;
